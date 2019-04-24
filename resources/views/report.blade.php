91e438bba4506d420e50b4a5cc7983dc911cfb97
bek <b.isakhmet@prodengi.kz>
2019-04-24 14:38:15 +0600

    Monolit

---
+++ resources/views/report.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Reports</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" type="text/css" href="{{asset('css/semantic.min.css')}}">
+        <script src="https://code.jquery.com/jquery-3.4.0.js" crossorigin="anonymous"></script>
+        <script src="{{asset('js/semantic.min.js')}}"></script>
+
+
+    </head>
+    <body>
+    <div class="ui secondary vertical menu">
+        <a class="active item">
+            Account
+        </a>
+        <a class="item">
+            Settings
+        </a>
+        <div class="ui dropdown item">
+            <i class="dropdown icon"></i>
+            Display Options
+            <div class="menu">
+                <div class="header">Text Size</div>
+                <a class="item">Small</a>
+                <a class="item">Medium</a>
+                <a class="item">Large</a>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
