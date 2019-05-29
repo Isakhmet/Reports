71fd56e7fb79eadd2048255f48215f519567af1d
bek <b.isakhmet@prodengi.kz>
2019-05-29 13:43:10 +0600

    PD-76 Report alfa bank

--- app/Classes/Reports/Oracle/AlfaBank.php
+++ app/Classes/Reports/Oracle/AlfaBank.php
@@ -4,6 +4,7 @@ namespace App\Classes\Reports\Oracle;
 
 use App\Classes\Connectors\Connectors;
 use App\Classes\Reports\Report;
+use Illuminate\Support\Arr;
 
 /**
  * Class AlfaBank
@@ -24,14 +25,28 @@ class AlfaBank extends Connectors implements Report
      */
     public function report($reportType, $page, $perPage, $from, $to)
     {
-        $connect = $this->connect($reportType);
-        $query   = $connect->table('alpha_bank_leads')
-                           ->where('created_at', '>=', $from . ' 00:00:00')
-                           ->where('created_at', '<=', $to . ' 23:59:59')
-                           ->paginate($perPage)
+        $connect  = $this->connect($reportType);
+        $query    = $connect->table('alpha_bank_leads')
+                            ->where('created_at', '>=', $from . ' 00:00:00')
+                            ->where('created_at', '<=', $to . ' 23:59:59')
+                            ->paginate($perPage)
         ;
-        $result  = json_decode(json_encode($query), true);
+        $result   = json_decode(json_encode($query), true);
+        $statuses = __('report.reports.alfaBank.statuses');
+        $cities   = __('report.reports.alfaBank.cities');
+        $columns  = __('report.reports.alfaBank.columns');
+        $keys     = array_keys($columns);
+        $headers  = array_values($columns);
 
-        return $result;
+        foreach ($result['data'] as $key => $row) {
+            $result['data'][$key]['status_id'] = $statuses[$row['status_id']];
+            $result['data'][$key]['towns']     = $cities[$row['towns']];
+            $result['data'][$key]              = Arr::only($result['data'][$key], $keys);
+        }
+
+        $array            = $result;
+        $array['headers'] = $headers;
+
+        return $array;
     }
 }
