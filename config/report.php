ecdc333ca5fa83d135440145aa56818e9d87bf0f
bek <b.isakhmet@prodengi.kz>
2019-07-16 11:48:16 +0600

    PD-76 Report score clients

--- config/report.php
+++ config/report.php
@@ -3,18 +3,19 @@
 return [
     'crm' => [
         'type' => 'db',
-        34 => \App\Classes\Reports\Crm\SendLeads::class,
-        35 => \App\Classes\Reports\Crm\LeadEffectReport::class,
-        60 => \App\Classes\Reports\Crm\CallTasks::class
+        34     => \App\Classes\Reports\Crm\SendLeads::class,
+        35     => \App\Classes\Reports\Crm\LeadEffectReport::class,
+        60     => \App\Classes\Reports\Crm\CallTasks::class
     ],
     'oracle' => [
-        'cluster' => App\Classes\Reports\Oracle\Cluster::class,
-        'alfa_bank' => App\Classes\Reports\Oracle\AlfaBank::class,
+        'cluster'      => App\Classes\Reports\Oracle\Cluster::class,
+        'alfa_bank'    => App\Classes\Reports\Oracle\AlfaBank::class,
         'score_points' => App\Classes\Reports\Oracle\ScorePoints\ScorePoints::class,
         'score_values' => App\Classes\Reports\Oracle\ScoreValues\ScoreValues::class,
-        'client' => App\Classes\Reports\Oracle\ScoreClients\ScoreClients::class,
-        'hcb_long' => App\Classes\Reports\Oracle\Hcb\Long\HcbLong::class,
-        'hcb_short' => App\Classes\Reports\Oracle\Hcb\Short\HcbShort::class,
+        'client'       => App\Classes\Reports\Oracle\ScoreClients\ScoreClients::class,
+        'hcb_long'     => App\Classes\Reports\Oracle\Hcb\Long\HcbLong::class,
+        'hcb_short'    => App\Classes\Reports\Oracle\Hcb\Short\HcbShort::class,
+        'agency'       => App\Classes\Reports\Oracle\Hcb\Short\HcbShort::class,
     ],
     'sms' => [
         'outgoing' => App\Classes\Reports\SmsCampaignManager\Outgoing::class,
