5522843b03df4ffd00146227435193035328daae
bek <b.isakhmet@prodengi.kz>
2019-08-22 15:15:53 +0600

    PD-76 Alfabank

--- app/Classes/Reports/Oracle/AlfaBank.php
+++ app/Classes/Reports/Oracle/AlfaBank.php
@@ -29,14 +29,28 @@ class AlfaBank extends Connectors implements Report
         $query    = $connect->table('alpha_bank_leads')
                             ->where('created_at', '>=', $from . ' 00:00:00')
                             ->where('created_at', '<=', $to . ' 23:59:59')
-                            ->paginate($perPage)
         ;
-        $result   = json_decode(json_encode($query), true);
+        $excelData = json_decode(
+            json_encode(
+                $query->get()
+                      ->toArray()
+            ), true
+        );
+        $result   = json_decode(json_encode($query->paginate($perPage)), true);
         $statuses = __('report.reports.alfaBank.statuses');
         $cities   = __('report.reports.alfaBank.cities');
         $columns  = __('report.reports.alfaBank.columns');
         $keys     = array_keys($columns);
         $headers  = array_values($columns);
+        $excel['columns'] = array_flip($columns);
+
+        foreach ($excelData as $key => $row) {
+            $excelData[$key]['status_id'] = $statuses[$row['status_id']];
+            $excelData[$key]['towns']     = $cities[$row['towns']];
+            $excelData[$key]              = Arr::only($excelData[$key], $keys);
+        }
+
+        $excel['data'] = $excelData;
 
         foreach ($result['data'] as $key => $row) {
             $result['data'][$key]['status_id'] = $statuses[$row['status_id']];
@@ -44,9 +58,9 @@ class AlfaBank extends Connectors implements Report
             $result['data'][$key]              = Arr::only($result['data'][$key], $keys);
         }
 
-        $array            = $result;
-        $array['headers'] = $headers;
+        $result['headers'] = $headers;
+        $result['excel']   = $excel;
 
-        return $array;
+        return $result;
     }
 }
